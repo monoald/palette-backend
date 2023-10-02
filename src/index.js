4889f78096c45d7a8ac506d1d5d6354aab236bf1
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-10-02 10:27:37 -0400

    [DEPLOY] prepare for appwrite deploy

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const https = require('https')
+const fs = require('fs')
+
+const app = require('./app')
+const { STATUS } = require('./config/config')
+
+if (STATUS === 'dev') {
+  const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+  }
+  
+  const server = https.createServer(options, app).listen(app.get('port'), () => {
+    console.log('https server running at port: ' + app.get('port'));
+  })
+} else {
+  app.listen(app.get('port'))
+  console.log('Server on port', app.get('port'))
+}
